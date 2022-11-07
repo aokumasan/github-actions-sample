f340c3857f8698d52ef49964ab6536f2596a1029
aokumasan <aokuma.dayo@gmail.com>
2022-11-07 14:24:15 +0900

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/aokumasan/github-actions-sample
 
 go 1.19
+
+require github.com/nifcloud/nifcloud-sdk-go v1.18.0
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.16.5 // indirect
+	github.com/aws/smithy-go v1.12.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/nifcloud/nifcloud-sdk-go/nifcloud"
+)
 
 func main() {
 	fmt.Println("Hello, GitHub!")
+	nif()
+}
+
+func nif() {
+	_ = nifcloud.NewConfig(
+		"YOUR_ACCESS_KEY_ID",
+		"YOUR_SECRET_ACCESS_KEY",
+		"jp-east-1",
+	)
 }
